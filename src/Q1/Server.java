b3b64c3b0caadd0e1b59ffec589b1ec2333f7a1d
goldylock <guslep@gmail.com>
2015-05-07 09:06:26 -0400

    init
    intial commit

---
+++ src/Q1/Server.java
@@ -0,0 +1,54 @@
+package Q1;
+
+import java.net.*;
+import java.io.*; 
+
+public class Server { 
+	public static void main(String[] args) throws IOException { 
+    
+		ServerSocket serverSocket = null; 
+
+		try { 
+			serverSocket = new ServerSocket(10118); 
+        } 
+		catch (IOException e) 
+        { 
+			System.err.println("On ne peut pas ecouter au  port: 10118.");
+			System.exit(1); 
+        } 
+
+		Socket clientSocket = null; 
+		System.out.println ("Le serveur est en marche, Attente de la connexion.....");
+
+		try { 
+			clientSocket = serverSocket.accept(); 
+        } 
+		catch (IOException e) 
+        { 
+			System.err.println("Accept a echoue.");
+			System.exit(1); 
+        } 
+
+		System.out.println ("connexion reussie");
+		System.out.println ("Attente de l'entree.....");
+
+		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); 
+		BufferedReader in = new BufferedReader(new InputStreamReader( clientSocket.getInputStream())); 
+
+		String inputLine;
+
+		while ((inputLine = in.readLine()) != null) 
+        { 
+			System.out.println ("Serveur: " + inputLine);
+			inputLine = inputLine.toUpperCase();
+        	out.println(inputLine);
+        	if (inputLine.equals("Bye.")) 
+        		break; 
+        } 
+
+		out.close(); 
+		in.close(); 
+		clientSocket.close(); 
+		serverSocket.close(); 
+	} 
+} 
